1f92aa1a5e2730c846d931b96c1864313d4f712e
Dom <dtaplin21@aol.com>
2023-09-12 22:15:56 -0700

    hey

--- server/app.js
+++ server/app.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const http = reuire("http")
+
+const app = express()
+
 const port = 5000;
+
+const server = http.createServer(app)
+ server.listen("port", () => {
+    console.log(`Hello from ${port}`)
+ })
